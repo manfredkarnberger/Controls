648ac0bd423d3a365860abd136a9db6a617a9b43
Manfred Karnberger <karnberger.manfred@gmail.com>
2025-07-27 19:34:25 +0200

    minor

---
+++ RoundedButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControls
+{
+    public class RoundedButton : Button
+    {
+        static RoundedButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundedButton),
+                new FrameworkPropertyMetadata(typeof(RoundedButton)));
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(RoundedButton),
+                new PropertyMetadata(new CornerRadius(10)));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
