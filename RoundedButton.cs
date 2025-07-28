63649353ee5a205dbc22454fba4b83a22da1f7dd
Manfred Karnberger <karnberger.manfred@gmail.com>
2025-07-28 08:13:14 +0200

    minor

--- RoundedButton.cs
+++ RoundedButton.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CustomControls
 {
@@ -11,6 +12,7 @@ namespace CustomControls
                 new FrameworkPropertyMetadata(typeof(RoundedButton)));
         }
 
+        // CornerRadius (bereits bekannt)
         public static readonly DependencyProperty CornerRadiusProperty =
             DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(RoundedButton),
                 new PropertyMetadata(new CornerRadius(10)));
@@ -19,6 +21,37 @@ namespace CustomControls
         {
             get => (CornerRadius)GetValue(CornerRadiusProperty);
             set => SetValue(CornerRadiusProperty, value);
+        }
+
+        // Icon (z. B. PNG oder DrawingImage)
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(ImageSource), typeof(RoundedButton),
+                new PropertyMetadata(null));
+
+        public ImageSource Icon
+        {
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register(nameof(IconWidth), typeof(double), typeof(RoundedButton),
+                new PropertyMetadata(16.0));
+
+        public double IconWidth
+        {
+            get => (double)GetValue(IconWidthProperty);
+            set => SetValue(IconWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty IconMarginProperty =
+            DependencyProperty.Register(nameof(IconMargin), typeof(Thickness), typeof(RoundedButton),
+                new PropertyMetadata(new Thickness(5, 0, 5, 0)));
+
+        public Thickness IconMargin
+        {
+            get => (Thickness)GetValue(IconMarginProperty);
+            set => SetValue(IconMarginProperty, value);
         }
     }
 }
--- obj/Debug/net8.0-windows/Controls.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Controls.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648ac0bd423d3a365860abd136a9db6a617a9b43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controls")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
